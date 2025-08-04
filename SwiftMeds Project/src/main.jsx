bbc16b669945259a1ead496769e823de102f412e
kEUMOE <keumoe@localhost.localdomain>
2025-08-04 11:25:15 +0100

    progress commit

--- SwiftMeds Project/src/main.jsx
+++ SwiftMeds Project/src/main.jsx
@@ -1,20 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
-import { ClerkProvider } from '@clerk/clerk-react'
-
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
-
-if (!PUBLISHABLE_KEY) {
-  throw new Error('Missing Publishable Key')
-}
+import App from './front-end/App.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
-    </ClerkProvider>
+      <App/>
   </StrictMode>,
 )
